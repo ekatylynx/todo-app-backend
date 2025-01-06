13f968ff205292e692d46b1d74f1590a1c31c900
Ekaterina <hitomizavr@gmail.com>
2025-01-07 00:51:07 +0300

    refactoring the project structure

--- todoProject/todos/validators.py
+++
@@ -1,5 +0,0 @@
-from django.core.exceptions import ValidationError
-
-def validate_title(value):
-    if not value.strip():  # Убираем пробелы и проверяем на пустую строку
-        raise ValidationError("Title cannot be empty!")
