e1b359fb3f4c044a74459955963e170e03e3db8e
Ekaterina <hitomizavr@gmail.com>
2025-01-07 00:44:11 +0300

    Update urls.py

--- todoProject/todos/urls.py
+++ todoProject/todos/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     re_path('signup/', views.signup_user), # Регистрация нового пользователя
     path('login/', CustomTokenObtainPairView.as_view(), name='login'),  # Вход
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Обновление access token
+    # path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Обновление access token
     path('logout/', LogoutView.as_view(), name='logout'),  # Выход
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),  # Сброс пароля
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
